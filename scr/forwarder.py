be7f12e1486088eb7c942e4290333e9130b5461c
myromeo <60740643+myromeo@users.noreply.github.com>
2026-03-17 12:19:38 +0000

    Update forwarder.py

--- scr/forwarder.py
+++ scr/forwarder.py
@@ -1,1 +1,10 @@
+import socket
+
+def send_udp(nmea_list, host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    for msg in nmea_list:
+        sock.sendto(msg.encode(), (host, port))
+
+    sock.close()
 
