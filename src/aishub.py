191a4495511cacf135f58642e1ad3eb35f80b363
myromeo <60740643+myromeo@users.noreply.github.com>
2026-03-17 13:44:19 +0000

    Update aishub.py

--- src/aishub.py
+++ src/aishub.py
@@ -1,8 +1,17 @@
 import requests
 from config import Config
+import logging
+logger = logging.getLogger("aishub2nmea")
 
 def fetch_ais_data():
     url = Config.build_url()
+    logger.debug(f"Fetching from URL: {url}")
+
     resp = requests.get(url, timeout=10)
+
+    logger.debug(f"AISHub response status: {resp.status_code}")
+    if resp.status_code != 200:
+        logger.error(f"AISHub returned status {resp.status_code}: {resp.text}")
+
     resp.raise_for_status()
     return resp.text
