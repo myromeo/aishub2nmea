012e6d27980e22605e6e056d121e3fae376483ff
myromeo <60740643+myromeo@users.noreply.github.com>
2026-03-17 13:45:01 +0000

    Update forwarder.py

--- src/forwarder.py
+++ src/forwarder.py
@@ -1,10 +1,14 @@
 import socket
+import logging
+logger = logging.getLogger("aishub2nmea")
 
 def send_udp(nmea_list, host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     for msg in nmea_list:
-        sock.sendto(msg.encode(), (host, port))
+        try:
+            sock.sendto(msg.encode(), (host, port))
+        except Exception as e:
+            logger.error(f"UDP send failed to {host}:{port}", exc_info=True)
 
     sock.close()
-
