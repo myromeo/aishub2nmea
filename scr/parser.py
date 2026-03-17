a56e2ca5974fb9f0cad4842e0618bcbc3ef5f04b
myromeo <60740643+myromeo@users.noreply.github.com>
2026-03-17 12:19:04 +0000

    Update parser.py

--- scr/parser.py
+++ scr/parser.py
@@ -1,1 +1,23 @@
+import xml.etree.ElementTree as ET
 
+def parse_aishub_xml(xml_string: str):
+    """
+    Returns: list of AIS vessel dicts
+    """
+    root = ET.fromstring(xml_string)
+    vessels = []
+
+    for v in root.findall("vessel"):
+        vessels.append({
+            "mmsi": v.attrib.get("MMSI"),
+            "lat": int(v.attrib.get("LATITUDE")) / 1_000_000,
+            "lon": int(v.attrib.get("LONGITUDE")) / 1_000_000,
+            "sog": float(v.attrib.get("SOG")) / 10,
+            "cog": float(v.attrib.get("COG")) / 10,
+            "heading": int(v.attrib.get("HEADING")),
+            "navstat": int(v.attrib.get("NAVSTAT")),
+            "rot": int(v.attrib.get("ROT")),
+            "accuracy": int(v.attrib.get("PAC", "0")),
+        })
+
+    return vessels
